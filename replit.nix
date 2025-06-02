18c0d81cbeb9f503f1295bf8a3f132ebb5e41dd5
Warren-code-sys <warren.aoummeur@gmail.com>
2025-06-02 18:13:24 +0200

    Fix npm command not found - configuration Replit corrigée

--- replit.nix
+++ replit.nix
@@ -2,7 +2,8 @@
   deps = [
     pkgs.nodejs-20_x
     pkgs.nodePackages.npm
-    pkgs.libuuid
-    pkgs.python3
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.replitPackages.jest
   ];
 } 
